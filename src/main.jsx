aa1eb6956db569cdc06ecde43a86c8e853d82cb9
Duc Bui <18020331@vnu.edu.vn>
2023-01-29 18:49:01 +0700

    test deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import Test from './Test'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
    <React.StrictMode>
       <BrowserRouter>
          <App />
+         <Test />
       </BrowserRouter>
    </React.StrictMode>
 )

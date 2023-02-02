5daf59fc52539c783c69e60a4502ae5ad6fd7df7
18020331vnu <18020331@vnu.edu.vn>
2023-02-02 21:01:10 +0700

    remove strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import './index.css'
 import Test from './Test'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-   <React.StrictMode>
-      <HashRouter>
-         {/* <BrowserRouter> */}
-         <App />
-         {/* </BrowserRouter> */}
-      </HashRouter>
-   </React.StrictMode>
+   <HashRouter>
+      {/* <BrowserRouter> */}
+      <App />
+      {/* </BrowserRouter> */}
+   </HashRouter>
 )

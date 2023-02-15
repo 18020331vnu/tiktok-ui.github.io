77bdf066e4a440c60e96b43ad0be9531e62ce321
18020331vnu <18020331@vnu.edu.vn>
2023-02-15 11:30:00 +0700

    add comment to video modal

---
+++ src/components/Video/Comment.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { LikeIcon, LikeIconFill } from '../Icons/VideoModalIcon/VideoModalIcon'
+import Button from '../Button/Button'
+import likeApi from '../../api/likeApi'
+
+function Comment({ user, data }) {
+   const {
+      comment: content,
+      id: commentId,
+      likes_count,
+      is_liked,
+      updated_at: lastUpdated,
+   } = data
+
+   const [liked, setliked] = useState(is_liked)
+   const [likeCount, setLikeCount] = useState(likes_count)
+   const { avatar, first_name, last_name, tick, nickname } = user
+   const formatDate = (lastUpdated) => {
+      const currentDate = new Date()
+      const toMiliSeconds = currentDate - lastUpdated
+      const toSeconds = toMiliSeconds / 1000
+      const toMinutes = toSeconds / 60
+      const toHours = toMinutes / 60
+      const toDays = toHours / 24
+
+      if (currentDate.getFullYear() !== lastUpdated.getFullYear()) {
+         return `${lastUpdated.getFullYear()}-${
+            lastUpdated.getMonth() + 1
+         }-${lastUpdated.getDate()}`
+      } else if (toDays > 3) {
+         return `${lastUpdated.getMonth() + 1}-${lastUpdated.getDate()}`
+      } else if (toDays > 1 && toDays < 3) {
+         return `${Math.floor(toDays)} ngày trước`
+      } else if (toHours > 1) {
+         return `${Math.floor(toHours)} giờ trước`
+      } else if (toMinutes > 1) {
+         return `${Math.floor(toMinutes)} phút trước`
+      } else if (toSeconds > 1) {
+         return `${Math.floor(toSeconds)} giây trước`
+      } else {
+         return 'Vừa xong'
+      }
+   }
+
+   const handleLikeComment = async (commentId) => {
+      await likeApi.likeComment(commentId)
+      setLikeCount(likeCount + 1)
+      setliked(true)
+   }
+
+   const handleUnlikeComment = async (commentId) => {
+      await likeApi.unlikeComment(commentId)
+      setLikeCount(likeCount - 1)
+      setliked(false)
+   }
+
+   try {
+      return (
+         <div className="mb-4 flex">
+            <img src={avatar} alt="" className="mr-3 h-10 w-10 rounded-full" />
+
+            <div className="flex-grow">
+               <p className="flex items-baseline text-lg font-bold leading-[25px]">
+                  {`${first_name} ${last_name}` === ' '
+                     ? nickname
+                     : `${first_name} ${last_name}`}
+                  {tick && <VerifyBagdeIcon className={'ml-1'} />}
+               </p>
+               <p className="mb-[6px] leading-[22px]">{content}</p>
+               <p className="text-sm text-[#16182380]">
+                  {formatDate(new Date(lastUpdated))}
+                  <span className="ml-6">Trả lời</span>
+               </p>
+            </div>
+
+            <div className="ml-[18px] flex w-6 flex-shrink-0 flex-col items-center pt-6">
+               {!liked && (
+                  <Button onClick={() => handleLikeComment(commentId)}>
+                     <LikeIcon />
+                  </Button>
+               )}
+               {liked && (
+                  <Button onClick={() => handleUnlikeComment(commentId)}>
+                     <LikeIconFill />
+                  </Button>
+               )}
+               <p className="text-xs leading-[17px]">{likeCount}</p>
+            </div>
+         </div>
+      )
+   } catch (error) {
+      console.log(error)
+   }
+}
+
+Comment.propTypes = {}
+
+export default Comment

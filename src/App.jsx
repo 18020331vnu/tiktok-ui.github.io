9f2f8329ec0d163ace1dd898ad0498c97c4732e4
18020331vnu <18020331@vnu.edu.vn>
2023-02-15 11:29:24 +0700

    setup route for video modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 // import { publicRoutes } from './routes/routes'
-import { Route, Routes } from 'react-router-dom'
+import { Outlet, Route, Routes, useLocation } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout'
 import NotFound from './pages/Error/Error'
 import Upload from './pages/Upload/Upload'
 import { defaultLayoutRoutes, headerOnlyRoutes } from './routes/routes'
 import Header from './components/Header/Header'
+import VideoModal from './components/Video/VideoModal'
+import Profile from './pages/Profile/Profile'
+import Home from './pages/Home/Home'
 
 function App() {
    return (
@@ -17,7 +20,14 @@ function App() {
                   key={route.path}
                   path={route.path}
                   element={<DefaultLayout>{route.element}</DefaultLayout>}
-               ></Route>
+               >
+                  {route.child ? (
+                     <Route
+                        path={route.child.path}
+                        element={route.child.element}
+                     />
+                  ) : null}
+               </Route>
             ))}
 
             {headerOnlyRoutes.map((route) => (
@@ -33,8 +43,9 @@ function App() {
                />
             ))}
 
-            <Route path="*" element={<NotFound />}></Route>
+            <Route path="*" element={<NotFound />} />
          </Routes>
+         <Outlet />
       </div>
    )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { memo, useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import Video from '../../components/Video/Video'
 import videoApi from '../../api/videoApi'
 import VideoLoading from '../../components/Video/VideoLoading'
+import { Outlet } from 'react-router-dom'
 
 function Home(props) {
    const [videoList, setVideoList] = useState([])
@@ -22,7 +23,6 @@ function Home(props) {
    }, [page])
 
    useEffect(() => {
-      let isLoading = false
       let observer = new IntersectionObserver((entires) => {
          if (entires[0].isIntersecting) {
             console.log(entires[0])
@@ -43,25 +43,28 @@ function Home(props) {
    })
 
    return (
-      <div>
-         {videoList.map((video, index) => {
-            if (index === videoList.length - 1)
+      <>
+         <div>
+            {videoList.map((video, index) => {
+               if (index === videoList.length - 1)
+                  return (
+                     <div ref={videoRef} key={video.id}>
+                        <Video data={video} />
+                     </div>
+                  )
                return (
-                  <div ref={videoRef} key={video.id}>
+                  <div key={video.id}>
                      <Video data={video} />
                   </div>
                )
-            return (
-               <div key={video.id}>
-                  <Video data={video} />
-               </div>
-            )
-         })}
-         <VideoLoading />
-      </div>
+            })}
+            <VideoLoading />
+         </div>
+         <Outlet />
+      </>
    )
 }
 
 Home.propTypes = {}
 
-export default Home
+export default memo(Home)

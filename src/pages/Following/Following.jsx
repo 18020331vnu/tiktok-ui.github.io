a833cfa2f0dd88522408e3fc28123568bb0779c5
18020331vnu <18020331@vnu.edu.vn>
2023-02-15 11:28:46 +0700

    make following page

--- src/pages/Following/Following.jsx
+++ src/pages/Following/Following.jsx
@@ -1,5 +1,70 @@
-function Following() {
-   return <div className="">Following</div>
+import React, { memo, useEffect, useRef, useState } from 'react'
+import PropTypes from 'prop-types'
+import Video from '../../components/Video/Video'
+import videoApi from '../../api/videoApi'
+import VideoLoading from '../../components/Video/VideoLoading'
+import { Outlet } from 'react-router-dom'
+
+function Following(props) {
+   const [videoList, setVideoList] = useState([])
+   const [page, setPage] = useState(1)
+   const videoRef = useRef(null)
+
+   useEffect(() => {
+      const getVideoList = async () => {
+         const response = await videoApi.getVideoList({
+            type: 'following',
+            page: page,
+         })
+         console.log(response.data)
+         setVideoList([...videoList, ...response.data])
+      }
+      getVideoList()
+   }, [page])
+
+   useEffect(() => {
+      let observer = new IntersectionObserver((entires) => {
+         if (entires[0].isIntersecting) {
+            console.log(entires[0])
+            videoRef.current = null
+            setPage(page + 1)
+         }
+      })
+
+      if (videoRef.current) {
+         observer.observe(videoRef.current)
+      }
+
+      return () => {
+         if (videoRef.current) {
+            observer.unobserve(videoRef.current)
+         }
+      }
+   })
+
+   return (
+      <>
+         <div>
+            {videoList.map((video, index) => {
+               if (index === videoList.length - 1)
+                  return (
+                     <div ref={videoRef} key={video.id}>
+                        <Video data={video} />
+                     </div>
+                  )
+               return (
+                  <div key={video.id}>
+                     <Video data={video} />
+                  </div>
+               )
+            })}
+            <VideoLoading />
+         </div>
+         <Outlet />
+      </>
+   )
 }
 
-export default Following
+Following.propTypes = {}
+
+export default memo(Following)
